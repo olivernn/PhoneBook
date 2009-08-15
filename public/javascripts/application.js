8e54d483ee37304164ed748c5e067d210b3d17b7
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-08-15 21:20:21 +0100

    Added basic styles, added inline labels to search field

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// inline form field labels for the search form
+$(document).ready(function(){
+	$('#search_form input').each(function(){
+		if($(this).val() === ''){
+			$(this).val($(this).attr('title'));
+		}
+		
+		$(this).focus(function(){
+			if($(this).val() === $(this).attr('title')){
+				$(this).val('').addClass('focused');
+			}
+		});
+		
+		$(this).blur(function(){
+			if($(this).val() === ''){
+				$(this).val($(this).attr('title')).removeClass('focused');
+			}
+		});
+	});
+});
